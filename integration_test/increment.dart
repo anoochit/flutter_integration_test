88199bfea7e1b1125ed3155730675aa137d21e9a
Anuchit Chalothorn <anoochit@gmail.com>
2023-01-14 18:03:28 +0700

    init

---
+++ integration_test/increment.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hello/main.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets(
+    "Increment test",
+    (tester) async {
+      // load my app
+      await tester.pumpWidget(const MyApp());
+      await tester.pumpAndSettle();
+
+      // Verify that our counter starts at 0.
+      expect(
+        find.text('0'),
+        findsOneWidget,
+        reason: '==> Counter must start at 0',
+      );
+      expect(
+        find.text('1'),
+        findsNothing,
+        reason: '==> Counter start at 0 must not be 1',
+      );
+
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump(const Duration(seconds: 1));
+
+      // Verify that our counter after tap + button
+      expect(find.text('0'), findsNothing,
+          reason: '==> Tap increment to 1 not 0');
+      expect(
+        find.text('1'),
+        findsOneWidget,
+        reason: '==> Tap increment to 1',
+      );
+
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump(const Duration(seconds: 1));
+
+      // Verify that our counter after tap + button
+      expect(find.text('2'), findsOneWidget, reason: '==> Tap increment to 2');
+
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump(const Duration(seconds: 1));
+
+      // Verify that our counter after tap + button
+      expect(find.text('3'), findsOneWidget, reason: '==> Tap increment to 3');
+    },
+  );
+}
